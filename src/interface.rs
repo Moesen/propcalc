08cce16bb4051d73b8b64eccfea915027c13e132
Moesen <gustavmoesmand@gmail.com>
2022-09-21 19:09:13 +0200

    added more stuffg

---
+++ src/interface.rs
@@ -0,0 +1,12 @@
+use inquire::{InquireError, Select};
+
+pub trait MenuInteraction {
+    
+}
+
+pub trait Menu {
+    fn functions(&self) -> str;
+}
+
+impl<T> MenuInteraction for T where T: Menu {
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 extern crate inquire;
 mod intro;
+mod interface;
+use inquire::{InquireError, Select};
+
 
 fn main() {
+    let options = ;
+    let ans: Result<&str, InquireError> =
+        Select::new("What number?", options).prompt();
+    match ans {
+        Ok(c) => println!("{}! That was mine too!", c),
+        Err(_) => println!("An error occured"),
+    };
 }
-
